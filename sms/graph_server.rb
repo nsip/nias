85a96e0a19b709e0217fe064f304f2e01c260a62
Nick Nicholas <opoudjis@optusnet.com.au>
2015-11-08 00:38:14 +1100

    Visualisation

---
+++ sms/graph_server.rb
@@ -0,0 +1,35 @@
+# graph_server.rb
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+
+require_relative 'sms_visualise_query'
+
+
+class GraphServer < Sinatra::Base
+
+
+get "/graph_data/linked_collections" do
+	id = params['id']
+	viz = SMSVizQuery.new
+        content_type 'application/json'
+	results = viz.find_linked_collections( id )
+	return results.to_json
+end
+
+get "/graph_data/attendance_per_class" do
+	viz = SMSVizQuery.new
+        content_type 'application/json'
+	results = viz.attendance_counts_per_class
+	return results.to_json
+end
+
+get "/graph_data/most_absent_students" do
+	viz = SMSVizQuery.new
+        content_type 'application/json'
+	results = viz.most_absent_students
+	return results.to_json
+end
+
+end
---
+++ sms/sms_visualise_query.rb
@@ -0,0 +1,87 @@
+# sms_visualise_query.rb
+require 'redis'
+require 'hashids'
+require 'date'
+require 'json'
+# query interface for the redis datasets to produce visualisation queries
+
+class SMSVizQuery
+
+	def initialize
+                @redis = Redis.new(:url => 'redis://localhost:6381', :driver => :hiredis)
+                @hashid = Hashids.new( 'nsip timesheeting' )
+                @expiry_seconds = 120 #update this for production
+                @rand_space = 10000
+        end
+
+	# counts ids in all collections that an id is connected to
+	def find_linked_collections( id )
+		results = []
+		collections = @redis.smembers('known:collections')
+		tally = {}
+		collections.each do |collection|
+			datapoints = @redis.sinter id, collection
+			datapoints = datapoints.reject{|x| x == id}
+			if datapoints.nil? or datapoints.empty?
+				# indirect links
+				tmp = @hashid.encode( rand(1...999) )
+                        	q = []
+                        	q = @redis.smembers id
+                        	next unless !q.empty? # return empty results if item not in db
+                               	@redis.sunionstore tmp, q.to_a
+                                results1 = @redis.sinter tmp, collection
+                                @redis.expire tmp, 5
+				tally[collection] = results1.size if !results1.nil? and !results.empty?
+			else
+				tally[collection] = datapoints.size
+			end
+		end
+		tally.each { | key, value| results << {:collection => key, :data => value } }
+		return results
+	end
+
+
+	# produce a count of attendances for all students
+	def attendance_counts
+		results = {}
+		students = @redis.smembers('StudentPersonal')
+		students.each do |s|
+			absences = @redis.sinter s, 'StudentDailyAttendance'
+			results[s] = absences.size
+		end
+		return results
+	end
+
+	# average counts of attendance for each class
+	def attendance_counts_per_class
+		absences_per_class = {}
+		results = []
+		attendances = attendance_counts()
+		students = attendances.keys
+		classes = @redis.smembers('TeachingGroup')
+		students.each do |student|
+			classes1 = @redis.sinter student, 'TeachingGroup'
+			classes1.each do |class1|
+				absences_per_class[class1] = [] if absences_per_class[class1].nil?
+				absences_per_class[class1] << attendances[student]
+			end
+		end
+		absences_per_class.each do |key, value|
+			results << {:class => key, "average absences" => value.inject(0.0) { |sum, el| sum + el } / value.size }
+		end
+		return results
+	end
+
+	# 20 most absent students
+	def most_absent_students
+		attendances = attendance_counts()
+		absentees = attendances.keys.sort { |x, y| attendances[y] <=> attendances[x] }
+		results = []
+		absentees[0..20].each do |x|
+			results << {:student => x, :absences => attendances[x] }
+		end
+		return results
+	end
+
+end
+
